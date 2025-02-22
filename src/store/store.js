4d1a44f233ccc8b806487082b3e429983a48f319
Rahul kumar <rahulramrishi298@gmail.com>
2025-02-22 21:20:09 +0530

    completed some basic part

---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+    reducer: {
+        // Add the generated reducer here
+    },
+});
+
+
+export default store;
